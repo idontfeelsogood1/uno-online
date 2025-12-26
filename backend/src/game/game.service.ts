19dec186a9d564a6b54d84defe4ab14d4cdecf20
Le Duong <leduongnewmemes123@gmail.com>
2025-12-26 19:20:48 +0700

    feat(backend): Implement get/set/remove methods for rooms

--- backend/src/game/game.service.ts
+++ backend/src/game/game.service.ts
@@ -1,4 +1,61 @@
 import { Injectable } from '@nestjs/common';
+import { GameRoom } from './class/game-room/GameRoom';
+import { GameBoard } from './class/game-board/GameBoard';
 
 @Injectable()
-export class GameService {}
+export class GameService {
+  private rooms: Map<string, GameRoom>;
+
+  public createRoom(
+    id: string,
+    name: string,
+    ownerId: string,
+    maxPlayer: number,
+  ): GameRoom {
+    return new GameRoom(id, name, ownerId, maxPlayer, new GameBoard(id));
+  }
+
+  public addRoom(room: GameRoom): void {
+    this.rooms.set(room.id, room);
+  }
+
+  public getRoom(roomId: string): GameRoom {
+    if (this.rooms.has(roomId)) return this.rooms.get(roomId)!;
+
+    const obj: object = Object.fromEntries(this.rooms);
+    throw new RoomNotFound(
+      `
+      roomId: ${roomId}
+      rooms: ${JSON.stringify(obj)}
+      `,
+      {},
+    );
+  }
+
+  public getAllRoom(): GameRoom[] {
+    return [...this.rooms.values()];
+  }
+
+  public removeRoom(roomId: string): GameRoom {
+    if (this.rooms.has(roomId)) {
+      const removedRoom: GameRoom = this.rooms.get(roomId)!;
+      this.rooms.delete(roomId);
+      return removedRoom;
+    }
+
+    const obj: object = Object.fromEntries(this.rooms);
+    throw new RoomNotFound(
+      `
+      roomId: ${roomId}
+      rooms: ${JSON.stringify(obj)}
+      `,
+      {},
+    );
+  }
+}
+
+export class RoomNotFound extends Error {
+  constructor(message: string, options: object) {
+    super(message, options);
+  }
+}
