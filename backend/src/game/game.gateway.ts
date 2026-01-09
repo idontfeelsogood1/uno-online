76bf2359b2e7f654cc95a5201e4dd70b4d1e159a
Le Duong <leduongnewmemes123@gmail.com>
2026-01-09 17:07:52 +0700

    feat(backend): Implement create-room, add validation data with ValidationPipe and emiting validation-error to the client with WsValidationFilter

--- backend/src/game/game.gateway.ts
+++ backend/src/game/game.gateway.ts
@@ -1,9 +1,50 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import {
+  SubscribeMessage,
+  WebSocketGateway,
+  MessageBody,
+  ConnectedSocket,
+} from '@nestjs/websockets';
+import { Socket } from 'socket.io';
+import { GameService } from './game.service';
+import { Player } from './class/player/Player';
+import { GameRoom } from './class/game-room/GameRoom';
+import { randomUUID } from 'crypto';
+import { ValidationPipe, UseFilters, UsePipes } from '@nestjs/common';
+import { CreateRoomDto } from './dto/create-room.dto';
+import { WsValidationFilter } from './filter/ws-validation.filter';
 
 @WebSocketGateway()
+@UseFilters(WsValidationFilter)
 export class GameGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
+  constructor(private readonly service: GameService) {}
+
+  @SubscribeMessage('create-room')
+  @UsePipes(new ValidationPipe())
+  async handleEvent(
+    @MessageBody() data: CreateRoomDto,
+    @ConnectedSocket() client: Socket,
+  ): Promise<string> {
+    try {
+      const { username, roomname, maxPlayers }: CreateRoomDto = data;
+      const playerSocketId: string = client.id;
+
+      const owner: Player = new Player(playerSocketId, username);
+      const room: GameRoom = this.service.createRoom(
+        randomUUID(),
+        roomname,
+        playerSocketId,
+        parseInt(maxPlayers),
+      );
+
+      await client.join(room.id);
+
+      this.service.addRoom(room);
+      this.service.addPlayerToRoom(room.id, owner);
+
+      return 'Successfully created and joined player to room';
+    } catch (err) {
+      console.log(err);
+      return 'Failed to create and joined player to room';
+    }
   }
 }
