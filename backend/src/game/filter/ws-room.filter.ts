66aea2af72c7c9985d6f5eaec0d9bede5a37f0a2
Le Duong <leduongnewmemes123@gmail.com>
2026-01-12 18:44:05 +0700

    feat(backend): Update filter

--- backend/src/game/filter/ws-room.filter.ts
+++ backend/src/game/filter/ws-room.filter.ts
@@ -6,9 +6,18 @@ import {
   RoomNotFound,
   RoomIsFull,
   RoomHasStarted,
+  PlayerNotInAnyRoom,
 } from '../game.service';
+import { PlayerNotFound } from '../class/game-room/GameRoom';
 
-@Catch(PlayerIsInARoom, RoomNotFound, RoomIsFull, RoomHasStarted)
+@Catch(
+  PlayerIsInARoom,
+  RoomNotFound,
+  RoomIsFull,
+  RoomHasStarted,
+  PlayerNotFound,
+  PlayerNotInAnyRoom,
+)
 export class WsRoomFilter extends BaseWsExceptionFilter {
   catch(exception: unknown, host: ArgumentsHost) {
     const client = host.switchToWs().getClient<Socket>();
@@ -20,22 +29,30 @@ export class WsRoomFilter extends BaseWsExceptionFilter {
     // Map Error Class -> Event Name & Message
     if (exception instanceof PlayerIsInARoom) {
       eventName = 'player-already-in-a-room';
-      message = 'Player is already in a room';
+      message = 'Player is already in a room.';
     }
     if (exception instanceof RoomNotFound) {
       eventName = 'room-not-found';
-      message = 'Room not found';
+      message = 'Room not found.';
     }
     if (exception instanceof RoomIsFull) {
       eventName = 'room-is-full';
-      message = 'Room is full';
+      message = 'Room is full.';
     }
     if (exception instanceof RoomHasStarted) {
       eventName = 'room-has-started';
-      message = 'Room has already started';
+      message = 'Room has already started.';
+    }
+    if (exception instanceof PlayerNotInAnyRoom) {
+      eventName = 'player-not-in-any-room';
+      message = 'Player is not in any room.';
+    }
+    if (exception instanceof PlayerNotFound) {
+      eventName = 'player-not-found';
+      message = 'Player not found.';
     }
 
     // Emit the specific event to the frontend
-    client.emit(eventName, { message });
+    client.emit(eventName, { message: message });
   }
 }
