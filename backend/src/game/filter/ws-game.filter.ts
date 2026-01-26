e66b00265193a2d7811e00274b8b0d9d8f6f0849
Le Duong <leduongnewmemes123@gmail.com>
2026-01-26 15:38:30 +0700

    feat(backend): Update filter

--- backend/src/game/filter/ws-game.filter.ts
+++ backend/src/game/filter/ws-game.filter.ts
@@ -1,9 +1,14 @@
 import { Catch, ArgumentsHost } from '@nestjs/common';
 import { BaseWsExceptionFilter } from '@nestjs/websockets';
 import { Socket } from 'socket.io';
-import { NotPlayerTurn } from '../game.service';
+import {
+  NotPlayerTurn,
+  CardsSentMustNotBeEmpty,
+  CannotUno,
+} from '../game.service';
 
-@Catch(NotPlayerTurn)
+// UNCAUGHT: AmountGreaterThanDrawPile, PlayerWon
+@Catch(NotPlayerTurn, CardsSentMustNotBeEmpty, CannotUno)
 export class WsGameFilter extends BaseWsExceptionFilter {
   catch(exception: unknown, host: ArgumentsHost) {
     const client = host.switchToWs().getClient<Socket>();
@@ -16,6 +21,14 @@ export class WsGameFilter extends BaseWsExceptionFilter {
     if (exception instanceof NotPlayerTurn) {
       eventName = 'not-player-turn';
       message = 'Not Player Turn.';
+    }
+    if (exception instanceof CardsSentMustNotBeEmpty) {
+      eventName = 'cards-sent-must-not-be-empty';
+      message = 'Cards send must not be empty.';
+    }
+    if (exception instanceof CannotUno) {
+      eventName = 'cannot-uno';
+      message = 'Cannot uno.';
     }
 
     // Emit the specific event to the frontend
