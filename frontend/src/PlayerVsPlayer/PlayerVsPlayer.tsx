3ae1079ac85f8163de6bb53e0003716cfe524669
Le Duong <leduongnewmemes123@gmail.com>
2026-02-09 20:11:53 +0700

    feat(frontend): Basic component implementation

--- frontend/src/PlayerVsPlayer/PlayerVsPlayer.tsx
+++ frontend/src/PlayerVsPlayer/PlayerVsPlayer.tsx
@@ -1,12 +1,23 @@
+import PlayerLobby from "../PlayerLobby/PlayerLobby";
+import Room from "../Room/Room";
+import Game from "../Game/Game";
+import { useState } from "react";
+
 interface PlayerVsPlayerProps {
   setHomeView: (view: null) => void;
 }
 
+// ESTABLISH A CONNECTION
 export default function PlayerVsPlayer({ setHomeView }: PlayerVsPlayerProps) {
-  return (
-    <dialog open>
-      <div>PlayerVsPlayer component</div>
-      <button onClick={() => setHomeView(null)}>Exit</button>
-    </dialog>
-  );
+  const [view, setView] = useState<string>("LOBBY");
+
+  if (view === "LOBBY") {
+    return <PlayerLobby setHomeView={setHomeView} setWrapperView={setView} />;
+  }
+  if (view === "ROOM") {
+    return <Room />;
+  }
+  if (view === "GAME") {
+    return <Game />;
+  }
 }
