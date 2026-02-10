c84193240f9f6bd29468dc9002635027a6997c97
Le Duong <leduongnewmemes123@gmail.com>
2026-02-10 16:11:40 +0700

    feat(backend): Enable HTTP request from multiple origins for dev and only 1 for prod

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,21 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  let originUrl: string;
+  if (process.env.NODE_ENV === 'dev') {
+    originUrl = '*';
+  } else {
+    originUrl = process.env.FRONTEND_URL!;
+  }
+
+  app.enableCors({
+    origin: originUrl,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+
+void bootstrap();
