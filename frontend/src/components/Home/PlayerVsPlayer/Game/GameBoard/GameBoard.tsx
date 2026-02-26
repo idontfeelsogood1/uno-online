3a1b07b9056dd76a251ba17050450b90a46ac849
LeDuong <leduongnewmemes123@gmail.com>
2026-02-26 18:40:29 +0700

    feat(frontend): Implement component

--- frontend/src/components/Home/PlayerVsPlayer/Game/GameBoard/GameBoard.tsx
+++ frontend/src/components/Home/PlayerVsPlayer/Game/GameBoard/GameBoard.tsx
@@ -1,3 +1,28 @@
-export default function GameBoard() {
-  return <div>GameBoard component</div>;
+import type { GameBoardProps } from "../../../../../types/commonTypes";
+import { getCardImgPath, getCardCoverImgPath } from "../../../../../api/helper";
+import { socket } from "../../../../../api/socket";
+
+export default function GameBoard({ topCard, enforcedColor }: GameBoardProps) {
+  return (
+    <div>
+      <button
+        onClick={() => {
+          socket.emit("draw-card");
+        }}
+      >
+        <img src={getCardCoverImgPath()} alt="Draw cards" />
+      </button>
+
+      <img
+        src={getCardImgPath(topCard)}
+        alt={topCard.color + " " + topCard.value}
+      />
+
+      <div>{topCard.color}</div>
+
+      <div>
+        {topCard.color === "BLACK" ? enforcedColor : "No enforced color"}
+      </div>
+    </div>
+  );
 }
