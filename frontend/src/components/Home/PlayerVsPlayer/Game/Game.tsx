5bec373148319174921abca0ec694ed16fe3c930
Le Duong <leduongnewmemes123@gmail.com>
2026-03-03 17:38:01 +0700

    feat(frontend): Add new props and provide GameAction for child components

--- frontend/src/components/Home/PlayerVsPlayer/Game/Game.tsx
+++ frontend/src/components/Home/PlayerVsPlayer/Game/Game.tsx
@@ -3,28 +3,32 @@ import CurrentPlayer from "./CurrentPlayer/CurrentPlayer";
 import GameBoard from "./GameBoard/GameBoard";
 import OtherPlayer from "./OtherPlayer/OtherPlayer";
 import { socket } from "../../../../api/socket";
+import { GameAction } from "../../../../api/GameAction";
 
-// PROVIDE GLOBAL STATE FOR gameState FOR CHILD COMPONENTS
-// RESET playHand AND MAYBE SOME OTHER VIEWS THROUGH actionType
-
-export default function Game({ gameState, actionSocketId }: GameProps) {
+export default function Game({
+  gameState,
+  actionType,
+  actionSocketId,
+}: GameProps) {
   return (
-    <div>
-      <GameBoard
-        topCard={gameState.topCard}
-        enforcedColor={gameState.enforcedColor}
-      />
-      {gameState.playerOrder.map((player) => {
-        if (player.socketId !== socket.id) {
-          return <OtherPlayer otherPlayer={player} />;
-        }
-      })}
-      {gameState.playerOrder.map((player) => {
-        if (player.socketId === socket.id) {
-          console.log("CurrentPlayer called");
-          return <CurrentPlayer player={player} />;
-        }
-      })}
-    </div>
+    <GameAction.Provider value={{ actionType, actionSocketId }}>
+      <div>
+        <GameBoard
+          topCard={gameState.topCard}
+          enforcedColor={gameState.enforcedColor}
+        />
+        {gameState.playerOrder.map((player) => {
+          if (player.socketId !== socket.id) {
+            return <OtherPlayer otherPlayer={player} />;
+          }
+        })}
+        {gameState.playerOrder.map((player) => {
+          if (player.socketId === socket.id) {
+            console.log("CurrentPlayer called");
+            return <CurrentPlayer player={player} />;
+          }
+        })}
+      </div>
+    </GameAction.Provider>
   );
 }
