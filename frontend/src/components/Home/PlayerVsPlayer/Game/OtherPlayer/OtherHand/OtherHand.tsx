6d4d03cfcd0c8f0f9987dca484001306a9438440
Le Duong <leduongnewmemes123@gmail.com>
2026-03-03 14:48:49 +0700

    feat(frontend): Implement component

--- frontend/src/components/Home/PlayerVsPlayer/Game/OtherPlayer/OtherHand/OtherHand.tsx
+++ frontend/src/components/Home/PlayerVsPlayer/Game/OtherPlayer/OtherHand/OtherHand.tsx
@@ -1,10 +1,12 @@
-import type { OtherHand } from "../../../../../../types/commonTypes";
+import type { OtherHandProps } from "../../../../../../types/commonTypes";
 import { getCardCoverImgPath } from "../../../../../../api/helper";
 
-export default function OtherHand({ otherHand }: OtherHand) {
-  {
-    return otherHand.map(() => {
-      return <img src={getCardCoverImgPath()} alt="Card cover" />;
-    });
-  }
+export default function OtherHand({ otherHand }: OtherHandProps) {
+  return (
+    <div>
+      {otherHand.map(() => {
+        return <img src={getCardCoverImgPath()} alt="Card cover" />;
+      })}
+    </div>
+  );
 }
