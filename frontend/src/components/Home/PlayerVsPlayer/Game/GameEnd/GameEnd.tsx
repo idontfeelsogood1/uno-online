690f8908d89094d2602d3d9d361f4ab45a120adf
Le Duong <leduongnewmemes123@gmail.com>
2026-03-16 17:01:50 +0700

    feat(frontend): Basic implementation

--- frontend/src/components/Home/PlayerVsPlayer/Game/GameEnd/GameEnd.tsx
+++ frontend/src/components/Home/PlayerVsPlayer/Game/GameEnd/GameEnd.tsx
@@ -1,3 +1,31 @@
-export default function GameEnd() {
-  return <div>GameEnd component</div>;
+import type { GameEndProps } from "../../../../../types/commonTypes";
+import { socket } from "../../../../../api/socket";
+
+export default function GameEnd({
+  players,
+  ownerSocketId,
+  setHomeView,
+  continueGame,
+}: GameEndProps) {
+  return (
+    <dialog open>
+      <h1>GAME ENDED</h1>
+      <div>
+        <ul>
+          {players.map((player) => {
+            return (
+              <li>
+                <span>{player.username}</span>
+                {socket.id === ownerSocketId && <span>(OWNER)</span>}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <div>
+        <button onClick={() => setHomeView}>HOME</button>
+        <button onClick={() => continueGame}>CONTINUE</button>
+      </div>
+    </dialog>
+  );
 }
