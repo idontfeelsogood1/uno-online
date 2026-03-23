80da9bdc1537a100dd27208be13ad95c5bd820d6
LeDuong <leduongnewmemes123@gmail.com>
2026-03-23 16:30:06 +0700

    feat(frontend): Add limit 7 to card covers of other player's hand

--- frontend/src/components/Home/PlayerVsPlayer/Game/OtherPlayer/OtherHand/OtherHand.tsx
+++ frontend/src/components/Home/PlayerVsPlayer/Game/OtherPlayer/OtherHand/OtherHand.tsx
@@ -2,11 +2,19 @@ import type { OtherHandProps } from "../../../../../../types/commonTypes";
 import { getCardCoverImgPath } from "../../../../../../api/helper";
 
 export default function OtherHand({ otherHand }: OtherHandProps) {
-  return (
-    <div>
-      {otherHand.map(() => {
-        return <img src={getCardCoverImgPath()} alt="Card cover" />;
-      })}
-    </div>
-  );
+  function renderHand(): React.ReactElement[] {
+    const htmlList: React.ReactElement[] = [];
+
+    for (let i = 0; i < 7; i++) {
+      if (otherHand[i] !== undefined) {
+        htmlList.push(<img src={getCardCoverImgPath()} alt="Card Cover"></img>);
+      } else {
+        return htmlList;
+      }
+    }
+
+    return htmlList;
+  }
+
+  return <div>{renderHand()}</div>;
 }
