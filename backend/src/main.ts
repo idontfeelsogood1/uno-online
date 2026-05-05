0dcd391e39fb9972d6e0231e3a34c4f92dc9d27f
LeDuong <leduongnewmemes123@gmail.com>
2026-05-05 08:44:44 +0700

    MAJOR: env config changes and github-runner now uses a PAT token to refresh authentication every time the service boots up

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,10 +5,10 @@ async function bootstrap() {
   const app = await NestFactory.create(GameModule);
 
   let originUrl: string;
-  if (process.env.NODE_ENV === 'dev') {
-    originUrl = '*';
-  } else {
+  if (process.env.NODE_ENV === 'production') {
     originUrl = process.env.FRONTEND_URL!;
+  } else {
+    originUrl = '*';
   }
 
   app.enableCors({
--- frontend/src/api/player-socket.ts
+++ frontend/src/api/player-socket.ts
@@ -1,10 +1,10 @@
 import { io } from "socket.io-client";
 
 let URL: string;
-if (import.meta.env.VITE_NODE_ENV === "dev") {
-  URL = "http://localhost:3000";
-} else {
+if (import.meta.env.VITE_NODE_ENV === "production") {
   URL = import.meta.env.VITE_API_URL;
+} else {
+  URL = "http://localhost:3000";
 }
 
 export const socket = io(`${URL}/PlayerVsPlayer`, {
