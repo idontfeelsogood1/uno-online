d7ca7ffd80472b4a2bf188eb0cd83e1235082ebb
Le Duong <leduongnewmemes123@gmail.com>
2026-06-06 01:23:06 +0700

    fix(frontend): Remove initial card distribution animation styling for other players where card stack on top instead of distributing

--- frontend/src/components/game/other-player/other-hand/OtherHand.tsx
+++ frontend/src/components/game/other-player/other-hand/OtherHand.tsx
@@ -4,7 +4,6 @@ import { useContext } from "react";
 import { GameInitialize } from "../../../../api/GameInitialize";
 // import { GameAction } from "../../../../api/GameAction";
 import { motion } from "motion/react";
-import { IsMobileView } from "../../../../api/IsMobileView";
 
 export default function OtherHand({
   otherHand,
@@ -12,7 +11,6 @@ export default function OtherHand({
   gridPositionIndex,
 }: OtherHandProps) {
   const initializeContext = useContext(GameInitialize);
-  const isMobileView = useContext(IsMobileView);
   // const actionContext = useContext(GameAction);
   const {
     cardContainerRef,
@@ -43,18 +41,8 @@ export default function OtherHand({
             zIndex: cardStyle.zIndex,
             width: cardStyle.width,
             height: cardStyle.height,
-            left:
-              initializeContext!.hasFinishedInitialAnimation || !isMobileView
-                ? isHorizontal
-                  ? cardStyle.calculatedPosition
-                  : "50%"
-                : "-10%",
-            top:
-              initializeContext!.hasFinishedInitialAnimation || !isMobileView
-                ? !isHorizontal
-                  ? cardStyle.calculatedPosition
-                  : "50%"
-                : "",
+            left: isHorizontal ? cardStyle.calculatedPosition : "50%",
+            top: !isHorizontal ? cardStyle.calculatedPosition : "50%",
             pointerEvents: "none",
           }}
           onAnimationComplete={() => {
